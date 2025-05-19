aa0b76e1915a70224159d4327b0a76452b66ed4d
Mark Legrove <mlegrove@myseneca.ca>
2025-05-19 15:06:21 -0400

    Initial commit of Lab 3 files

---
+++ lab3a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# return_text_value() function
+# Author ID: mlegrove
+
+def hello():
+    print('Hello World')
+    print('Inside a Function')
+
+def return_text_value():
+    name = 'Terry'
+    greeting = 'Good Morning ' + name 
+    return greeting
+
+def return_number_value():
+    num1 = 10
+    num2 = 5
+    num3 = num1 + num2
+    return num3
+
+if __name__ == '__main__':
+    print('python code')
+    text = return_text_value()
+    print(text)
+    number = return_number_value()
+    print(str(number))
+
